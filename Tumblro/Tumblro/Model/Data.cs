a09d135d62acee534b10d8b38f9a5081e716911d
Jan Walczak <jw@praksis.pl>
2021-12-29 03:32:47 +0100

    Added simple class summary

--- Tumblro/Tumblro/Model/Data.cs
+++ Tumblro/Tumblro/Model/Data.cs
@@ -4,6 +4,8 @@ using System.Text;
 
 namespace Tumblro
 {
+    /// <summary>Class <c>Data</c> is first tag from tumblr json file</summary>
+    ///
     public class Data
     {
         public Meta meta { get; set; }
